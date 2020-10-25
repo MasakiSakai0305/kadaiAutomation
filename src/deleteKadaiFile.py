7ca6a332286f2c98cdb30c70ff11b86176a29fa5
masa_Lab <ma20043@shibaura-it.ac.jp>
2020-10-25 18:27:06 +0900

    [minor update] 表示の見やすさを改善
    削除するファイル数の表示を追加, 見やすいように改行を追加

--- src/deleteKadaiFile.py
+++ src/deleteKadaiFile.py
@@ -2,12 +2,13 @@ import os
 import glob
 
 def remove_glob(pathname, recursive=True):
-    print("削除するファイルのパス: {}".format(pathname))
+    print("\n削除するファイルのパス: {}".format(pathname))
+    print("削除するファイルの数: {}\n".format(len(glob.glob(pathname, recursive=recursive))))
     for p in glob.glob(pathname, recursive=recursive):
         if os.path.isfile(p):
             print("削除: {}".format(p))
             os.remove(p)
-            
+          
 if __name__ == "__main__":
     
     jugyoNum = input("削除する授業の回を入力: ")
